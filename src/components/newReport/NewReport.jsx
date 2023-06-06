92bc0a113ca86e38a8bddd9b2585988cb0e55952
berrubino <berni.rubino@gmail.com>
2023-06-06 13:50:43 -0300

    feat:vista de ReportViews con estilos, botones,imagen

---
+++ src/components/newReport/NewReport.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { MainLayout } from "../layout/MainLayout";
+import ButtonGlobant from "../commons/ButtonGlobant";
+import { Box, Typography, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import newReportImg from "../../assets/newReportImg.png";
+
+function NewReport() {
+  const user = useSelector((state) => state.user);
+
+  return (
+    <MainLayout title="newReport" inLoginOrRegister={true}>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        overflow="overflow"
+      >
+        <Typography variant="body1" sx={{ mb: 2 }}>
+          ¡Hola, {user.name}! Bienvenido/a.
+        </Typography>
+        <Typography variant="body1" sx={{ mb: 1 }}>
+          Have a problem with your kit?
+        </Typography>
+        <Typography variant="body1" sx={{ mb: 3 }}>
+          Report your problem with help desk.
+        </Typography>
+
+        <Box mb={3}>
+          <img src={newReportImg} alt="Imagen" style={{ maxWidth: "100%" }} />
+        </Box>
+        <Box width="50%" mb={1}>
+          <ButtonGlobant>Report an issue</ButtonGlobant>
+        </Box>
+        <Box width="50%" mb={1}>
+          <ButtonGlobant>My Profile</ButtonGlobant>
+        </Box>
+        <Box width="50%" mb={3}>
+          <ButtonGlobant>Admin View</ButtonGlobant>
+        </Box>
+      </Box>
+    </MainLayout>
+  );
+}
+
+export default NewReport;
