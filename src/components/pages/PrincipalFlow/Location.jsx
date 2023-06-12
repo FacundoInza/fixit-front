310175d6391791a8374dbd97ee360a709339d549
inza01-git <facundoinza5@gmail.com>
2023-06-12 17:05:52 -0300

    first test with leaflet

--- src/components/pages/PrincipalFlow/Location.jsx
+++ src/components/pages/PrincipalFlow/Location.jsx
@@ -1,47 +1,67 @@
+//REACT
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+//MATERIAL
+import { Box, Typography } from "@mui/material";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "./map.css";
+//LOCAL COMPONENTS
 import { MainLayout } from "../../layout/MainLayout";
 import ButtonGlobant from "../../commons/ButtonGlobant";
-import { Box, Card, CardContent, Typography } from "@mui/material";
-
-import mapImg from "../../../assets/mapImg.png";
-
-import { useDispatch, useSelector } from "react-redux";
-import { axiosGetNearbyOffice, axiosUpdateUser } from "../../../services/api";
-import { getLocation } from "../../../utils";
-import { updateUser } from "../../../store/users";
-import MapContainer from "../../commons/Map";
 import CardOffice from "../../commons/Cards/CardOffice";
 
+//AXIOS
+import { axiosGetNearbyOffice, axiosUpdateUser } from "../../../services/api";
+import { getLocation } from "../../../utils";
+//ACTIONS
+import { updateUser } from "../../../store/users";
+
 function Location() {
-  const dispatch = useDispatch();
-  const [nearbyOffices, setNearbyOffices] = useState([]);
   const { id, location } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const [nearbyOffices, setNearbyOffices] = useState([]);
   const [markers, setMarkers] = useState("");
 
   useEffect(() => {
-    const locationUpdate = async () => {
-      const { error, data } = await getLocation();
-
-      const lat = String(data.coords.latitude);
-      const lng = String(data.coords.longitude);
-
-      const { message } = await axiosUpdateUser({ location: [lat, lng] }, id);
-      dispatch(updateUser({ location: [lat, lng] }));
-      const offices = await axiosGetNearbyOffice({ lat, lng });
-
-      const stringMarkers = offices
-        .map(
-          (office) =>
-            `${office.geometry.location.lat},${office.geometry.location.lng}`
-        )
-        .join("|");
-      console.log("hola", stringMarkers);
-      setMarkers(stringMarkers);
-      console.log(offices);
-      setNearbyOffices(offices);
-    };
     locationUpdate();
   }, []);
+
+  const locationUpdate = async () => {
+    await setUserLocation();
+    await setOfficesLocation();
+  };
+
+  const setUserLocation = async () => {
+    const { error, data } = await getLocation();
+
+    const lat = data.coords.latitude;
+    const lng = data.coords.longitude;
+
+    const { message } = await axiosUpdateUser({ location: [lat, lng] }, id);
+
+    dispatch(updateUser({ location: [lat, lng] }));
+  };
+
+  const setOfficesLocation = async () => {
+    const offices = await axiosGetNearbyOffice({
+      lat: location[0],
+      lng: location[1],
+    });
+    console.log(offices);
+    setNearbyOffices(offices);
+  };
+
+  const setOfficesMarkers = (offices) => {
+    const stringMarkers = offices
+      .map(
+        (office) =>
+          `${office.geometry.location.lat},${office.geometry.location.lng}`
+      )
+      .join("|");
+
+    setMarkers(stringMarkers);
+  };
 
   return (
     <MainLayout title="Login" inLoginOrRegister={true}>
@@ -60,9 +80,21 @@ function Location() {
             We identified you are near the following office
           </Typography>
 
-          <img
-            src={`https://maps.googleapis.com/maps/api/staticmap?center=${location[0]},${location[1]}&zoom=13&size=400x400&markers=color:red|${markers}&key=AIzaSyBk7HcRvPnJ1jDgWzRvlcdpCui4MlHpRl4`}
-          />
+          <MapContainer
+            center={[51.505, -0.09]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
 
           {nearbyOffices &&
             nearbyOffices.map((office, i) => (
