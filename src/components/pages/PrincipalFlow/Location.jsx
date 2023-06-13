a313e23028bb4306c68bb7c3903a02d9833898b1
inza01-git <facundoinza5@gmail.com>
2023-06-12 22:21:02 -0300

    feat: Geolocation offices's with leaflet and config Offices cards

--- src/components/pages/PrincipalFlow/Location.jsx
+++ src/components/pages/PrincipalFlow/Location.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 //MATERIAL
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Grid, Rating, Typography } from "@mui/material";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "./map.css";
 //LOCAL COMPONENTS
@@ -20,8 +20,10 @@ function Location() {
   const { id, location } = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
-  const [nearbyOffices, setNearbyOffices] = useState([]);
-  const [markers, setMarkers] = useState("");
+  const [nearbyOffices, setNearbyOffices] = useState("");
+  const [selectedOffice, setSelectedOffice] = useState("");
+
+  console.log("office", selectedOffice);
 
   useEffect(() => {
     locationUpdate();
@@ -48,63 +50,92 @@ function Location() {
       lat: location[0],
       lng: location[1],
     });
-    console.log(offices);
     setNearbyOffices(offices);
-  };
-
-  const setOfficesMarkers = (offices) => {
-    const stringMarkers = offices
-      .map(
-        (office) =>
-          `${office.geometry.location.lat},${office.geometry.location.lng}`
-      )
-      .join("|");
-
-    setMarkers(stringMarkers);
+    setSelectedOffice(offices[0]);
   };
 
   return (
     <MainLayout title="Login" inLoginOrRegister={true}>
-      <div style={{ maxHeight: "calc(100vh - 250px)", overflowY: "auto" }}>
+      <div
+        style={{
+          maxHeight: "calc(100vh - 250px)",
+          overflowY: "auto",
+          overflowX: "hidden",
+        }}
+      >
         <Box
           display="flex"
           justifyContent="center"
           alignItems="center"
           flexDirection="column"
-          overflow="overflow"
         >
-          <Typography variant="body1" sx={{ mb: 1 }} fontWeight="bold">
-            Location:
-          </Typography>
-          <Typography variant="body1" sx={{ mb: 3 }}>
-            We identified you are near the following office
-          </Typography>
-
-          <MapContainer
-            center={[51.505, -0.09]}
-            zoom={13}
-            scrollWheelZoom={false}
+          <Box
+            display={"flex"}
+            flexDirection={"column"}
+            padding={2}
+            alignItems={"center"}
           >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.505, -0.09]}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          </MapContainer>
+            <Typography variant="body1" sx={{ mb: 1 }} fontWeight="bold">
+              Location:
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 3 }}>
+              We identified you are near the following office
+            </Typography>
+          </Box>
 
-          {nearbyOffices &&
-            nearbyOffices.map((office, i) => (
-              <CardOffice key={i} office={office} />
-            ))}
+          {nearbyOffices && (
+            <MapContainer
+              style={{ width: "100%", height: "500px" }}
+              center={[
+                selectedOffice.geometry.location.lat,
+                selectedOffice.geometry.location.lng,
+              ]}
+              zoom={15}
+              scrollWheelZoom={false}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
 
-          <Box width="50%" mb={1}>
+              {nearbyOffices.map((office, i) => {
+                const lat = office.geometry.location.lat;
+                const lng = office.geometry.location.lng;
+                return (
+                  <Marker key={i} position={[lat, lng]}>
+                    <Popup>
+                      <h1>{office.name}</h1>
+                      <h2>Address: {office.formatted_address.split(",")[0]}</h2>
+                      <Rating name="read-only" value={office.rating} readOnly />
+                      {office.opening_hours.open_now ? (
+                        <p style={{ color: "green" }}>Is Open</p>
+                      ) : (
+                        <p style={{ color: "red" }}>Is Close</p>
+                      )}
+                    </Popup>
+                  </Marker>
+                );
+              })}
+            </MapContainer>
+          )}
+
+          <Grid container spacing={2} width={"70%"} margin={5}>
+            {nearbyOffices &&
+              nearbyOffices.map((office, i) => (
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                  <CardOffice
+                    office={office}
+                    setSelectedOffice={setSelectedOffice}
+                    selectedOffice={selectedOffice}
+                  />
+                </Grid>
+              ))}
+          </Grid>
+
+          <Box width="50%" m={5}>
             <ButtonGlobant>Confirm Office</ButtonGlobant>
           </Box>
-          <Box width="50%" mb={3} display={"none"}>
+          <Box width="50%" m={2} display={"none"}>
             <ButtonGlobant>Choose Another Office</ButtonGlobant>
           </Box>
         </Box>
