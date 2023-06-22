e5e6fc3d1e8ee65764967e017b404d9744bc0cbc
julianbesteiro <julianbesteiro1@gmail.com>
2023-06-22 12:09:37 -0300

    fix: highlight offices cards and fix cases filter

--- src/services/api.js
+++ src/services/api.js
@@ -46,6 +46,7 @@ export const axiosCasesUser = async (id, filterAds) => {
         withCredentials: true,
       }
     );
+
     return data;
   } catch (error) {
     console.log(error);
@@ -55,6 +56,10 @@ export const axiosCasesUser = async (id, filterAds) => {
 export const axiosUpdateUser = async (update, id) => {
   try {
     const { data } = await axios.put(`${apiUrl}users/update/${id}`, update);
+
+    document.cookie = "token=" + data;
+
+    return { message: data };
   } catch (error) {
     throw new Error({ message: "The user was not updated" });
   }
